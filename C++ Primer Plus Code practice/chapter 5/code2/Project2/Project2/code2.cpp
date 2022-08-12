854d0af86cc7bdba4b1e2a1107ba13fc54b10726
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-08-12 19:50:39 +0800

    Add files via upload

---
+++ C++ Primer Plus Code practice/chapter 5/code2/Project2/Project2/code2.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+//5.8∏¥œ∞Ã‚
+//6.
+#if 1
+int main(void)
+{
+	for (int i = 1; i <= 64; i *= 2)
+		std::cout << i << '\t';
+
+	return 0;
+}
+#endif
