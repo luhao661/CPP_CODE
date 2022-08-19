1fc80e8c7a2142f88ec8b122526b5f8dd796d377
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-08-19 18:54:46 +0800

    Add files via upload

--- C++ Primer Plus Code practice/chapter 7/code1/Project1/Project1/code1.cpp
+++ C++ Primer Plus Code practice/chapter 7/code1/Project1/Project1/code1.cpp
@@ -208,8 +208,9 @@ int sum_arr(int arr[], int n)
 #endif
 
 
-//程序清单7.7 填充数组、保护和显示数组、修改数组
-#if 1
+//程序清单7.7 填充数组、保护和显示数组、修改数组的函数
+//将数组起始处的指针作为一个参数，将数组长度作为第二个参数
+#if 0
 const int Max = 5;
 
 // function prototypes
@@ -289,5 +290,235 @@ void revalue(double r, double ar[], int n)
 {
     for (int i = 0; i < n; i++)
         ar[i] *= r;
+}
+#endif
+
+
+//程序清单7.8 另一种给函数提供所需信息的方法
+//一个指针标识数组的开头，一个指针标识数组结尾后面的一个位置
+#if 0
+const int ArSize = 8;
+
+int sum_arr(const int* begin, const int* end);
+
+int main()
+{
+    using namespace std;
+    int cookies[ArSize] = { 1,2,4,8,16,32,64,128 };
+
+    int sum = sum_arr(cookies, cookies + ArSize);
+    cout << "Total cookies eaten: " << sum << endl;
+
+    sum = sum_arr(cookies, cookies + 3);        // first 3 elements
+    cout << "First three eaters ate " << sum << " cookies.\n";
+
+    sum = sum_arr(cookies + 4, cookies + 8);    // last 4 elements
+    cout << "Last four eaters ate " << sum << " cookies.\n";
+    // cin.get();
+    return 0;
+}
+
+// return the sum of an integer array
+int sum_arr(const int* begin, const int* end)
+{
+    const int* pt;
+    int total = 0;
+
+    for (pt = begin; pt != end; pt++)
+        total = total + *pt;
+    return total;
+}
+#endif
+
+
+//程序清单7.9 用函数处理字符串
+#if 0
+unsigned int c_in_str(const char* str, char ch);
+
+int main()
+{
+    using namespace std;
+    char mmm[15] = "minimum";    // string in an array
+// some systems require preceding char with static to
+// enable array initialization
+
+    const char* wail = "ululate";    // wail points to string
+
+    unsigned int ms = c_in_str(mmm, 'm');
+    unsigned int us = c_in_str(wail, 'u');
+
+    cout << ms << " m characters in " << mmm << endl;
+    cout << us << " u characters in " << wail << endl;
+    // cin.get();
+    return 0;
+}
+
+//不必将字符串长度作为参数传递给函数
+unsigned int c_in_str(const char* str, char ch)
+{
+    unsigned int count = 0;
+
+    while (*str)        // quit when *str is '\0'
+    {
+        if (*str == ch)
+            count++;
+        str++;        // move pointer to next char
+    }
+    return count;
+}
+#endif
+
+
+//程序清单7.10 返回C风格字符串的函数
+#if 0
+char* buildstr(char c, int n);     // prototype
+
+int main()
+{
+    using namespace std;
+    int times;
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+    cout << "Enter an integer: ";
+    cin >> times;
+
+    char* ps = buildstr(ch, times);
+    cout << ps << endl;
+    delete[] ps;                   // free memory
+
+    ps = buildstr('+', 20);         // reuse pointer
+    cout << ps << "-DONE-" << ps << endl;
+    delete[] ps;                   // free memory
+    // cin.get();
+    // cin.get();
+    return 0;
+}
+
+// builds string made of n c characters
+char* buildstr(char c, int n)
+{
+    char* pstr = new char[n + 1];
+
+    while (n-- > 0)
+        pstr[n] = c;        // fill rest of string
+
+    pstr[n] = '\0';         // terminate string
+
+    return pstr;//***注***此函数结束时，指针pstr所占的内存将被释放，
+}                      //而new申请的动态分配的内存空间不会被释放,
+                       //由于函数返回了指针pstr上存的地址，因此main()仍可以用ps来访问创建的字符串
+#endif  
+
+
+//程序清单7.11 传递并返回结构
+#if 0
+struct travel_time
+{
+    int hours;
+    int mins;
+};
+
+const int Mins_per_hr = 60;
+
+travel_time sum(travel_time t1, travel_time t2);
+void show_time(travel_time t);
+
+int main()
+{
+    using namespace std;
+    travel_time day1 = { 5, 45 };    // 5 hrs, 45 min
+    travel_time day2 = { 4, 55 };    // 4 hrs, 55 min
+
+    travel_time trip = sum(day1, day2);
+    cout << "Two-day total: ";
+    show_time(trip);
+
+    travel_time day3 = { 4, 32 };
+    cout << "Three-day total: ";
+    show_time(sum(trip, day3));
+    // cin.get();
+
+    return 0;
+}
+
+travel_time sum(travel_time t1, travel_time t2)
+{
+    travel_time total;
+
+    total.mins = (t1.mins + t2.mins) % Mins_per_hr;
+    total.hours = t1.hours + t2.hours +
+        (t1.mins + t2.mins) / Mins_per_hr;
+    return total;
+}
+
+void show_time(travel_time t)
+{
+    using namespace std;
+    cout << t.hours << " hours, "
+        << t.mins << " minutes\n";
+}
+#endif
+
+
+//程序清单7.12 传递并返回结构  实际应用
+#if 1
+#include <cmath>//sqrt()、atan2()
+
+// structure declarations
+struct polar
+{
+    double distance;      // distance from origin
+    double angle;         // direction from origin
+};
+struct rect
+{
+    double x;             // horizontal distance from origin
+    double y;             // vertical distance from origin
+};
+
+// prototypes
+polar rect_to_polar(rect xypos);
+void show_polar(polar dapos);
+
+int main()
+{
+    using namespace std;
+    rect rplace;
+    polar pplace;
+
+    cout << "Enter the x and y values: ";
+    while (cin >> rplace.x >> rplace.y)  // slick use of cin
+    {
+        pplace = rect_to_polar(rplace);
+        show_polar(pplace);
+        cout << "Next two numbers (q to quit): ";
+    }
+    cout << "Done.\n";
+    return 0;
+}
+
+// convert rectangular to polar coordinates
+polar rect_to_polar(rect xypos)
+{
+    using namespace std;
+    polar answer;
+
+    answer.distance =
+        sqrt(xypos.x * xypos.x + xypos.y * xypos.y);
+    answer.angle = atan2(xypos.y, xypos.x);
+    return answer;      // returns a polar structure
+}
+
+// show polar coordinates, converting angle to degrees
+void show_polar(polar dapos)
+{
+    using namespace std;
+    const double Rad_to_deg = 57.29577951;
+
+    cout << "distance = " << dapos.distance;
+    cout << ", angle = " << dapos.angle * Rad_to_deg;
+    cout << " degrees\n";
 }
 #endif
