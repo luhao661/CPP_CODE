1722c22605094a5aad5fa05388e8fabb747fe1cc
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-09-10 13:09:19 +0800

    Add files via upload

--- C++ Primer Plus Code practice/chapter 10/code2/Project2/Project2/10.9.5.h
+++ C++ Primer Plus Code practice/chapter 10/code2/Project2/Project2/10.9.5.h
@@ -6,12 +6,14 @@
 class Account
 {
 	static const int LEN = 50;//创建LEN常量，不存储对象中
-	char m_name[LEN];
+	char m_name[LEN];//添加m_前缀以避免方法的形参名与类的数据成员名相同
 	enum {LLEN=80};//创建符号常量
 	char m_identification[LLEN];
 	double m_savings;
 public:
 	Account(const char *name,const char *identification,double savings);//声明一个构造函数
+	/****注***以下语句与上述语句功能相同
+	void init_account(const char *name,const char*identification,double savings);*/
 	Account();//声明一个默认构造函数
 	~Account();//声明一个析构函数
 	void show() const;
