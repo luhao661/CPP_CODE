11cd8d578f6ccf7f934f5a8c10744bff72ef3e4a
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-09-24 19:04:38 +0800

    Add files via upload

--- C++ Primer Plus Code practice/chapter 12/code1/Project1/Project1/code1.cpp
+++ C++ Primer Plus Code practice/chapter 12/code1/Project1/Project1/code1.cpp
@@ -1,10 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
 
 //程序清单12.3 研究一个不完善的类
 //链接12.2.cpp
 #if 0
 #include "cxqd12.1.h"
-#include <iostream>
 using std::cout;
 using std::endl;
 
@@ -324,8 +325,106 @@ int main()
 #endif
 
 
-//
-//链接
-#if 1
+//程序清单12.12 
+//链接cxqd12.11.cpp
+#if 0
+#include "cxqd12.10.h"
+#include <cstdlib> // for rand() and srand()
+#include <ctime>   // for time()
 
+const int MIN_PER_HR = 60;
+
+bool newcustomer(double x); // is there a new customer?
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    using std::ios_base;
+    // setting things up
+    std::srand(std::time(0));    //  random initializing of rand()
+
+    cout << "Case Study: Bank of Heather Automatic Teller\n";
+    cout << "Enter maximum size of queue: ";
+    int qs;
+    cin >> qs;
+    Queue line(qs);                 //创建对象并用构造函数初始化
+                                              // line queue holds up to qs people
+
+    cout << "Enter the number of simulation hours: ";
+    int hours;              //  hours of simulation
+    cin >> hours;
+    // simulation will run 1 cycle per minute
+    long cyclelimit = MIN_PER_HR * hours; // # of cycles
+
+    cout << "Enter the average number of customers per hour: ";
+    double perhour;         //  average # of arrival per hour
+    cin >> perhour;
+    double min_per_cust;    //  average time between arrivals
+    min_per_cust = MIN_PER_HR / perhour;//得每位客户到来的平均时间
+
+    Item temp;              //  new customer data
+    long turnaways = 0;     //  turned away by full queue
+    long customers = 0;     //  joined the queue
+    long served = 0;        //  served during the simulation
+    long sum_line = 0;      //  cumulative line length
+    int wait_time = 0;      //  time until autoteller is free
+    long line_wait = 0;     //  cumulative time in line
+
+
+// running the simulation
+    for (int cycle = 0; cycle < cyclelimit; cycle++)
+    {
+        if (newcustomer(min_per_cust))  // have newcomer
+        {
+            if (line.isfull())
+                turnaways++;
+            else
+            {
+                customers++;
+                temp.set(cycle);    // cycle = time of arrival
+                line.enqueue(temp); // add newcomer to line
+            }
+        }
+        if (wait_time <= 0 && !line.isempty())//当队列空闲且有节点时
+        {
+            line.dequeue(temp);     //排第一个的顾客在队列中被排除，其开始进行交易
+            wait_time = temp.ptime(); //wait_time即进行交易所花的时间
+
+            line_wait += cycle - temp.when();
+            served++;
+        }
+        if (wait_time > 0)
+            wait_time--;
+        sum_line += line.queuecount();
+    }
+
+    // reporting results
+    if (customers > 0)
+    {
+        cout << "customers accepted: " << customers << endl;//加入队列的人数
+        cout << "  customers served: " << served << endl;//已服务完的人数
+        cout << "         turnaways: " << turnaways << endl;
+        cout << "average queue size: ";
+        cout.precision(2);
+        cout.setf(ios_base::fixed, ios_base::floatfield);
+        cout << (double)sum_line / cyclelimit << endl;
+        cout << " average wait time: "
+            << (double)line_wait / served << " minutes\n";
+    }
+    else
+        cout << "No customers!\n";
+    cout << "Done!\n";
+    // cin.get();
+    // cin.get();
+    return 0;
+}
+
+//  x = average time, in minutes, between customers
+//  return value is true if customer shows up this minute
+bool newcustomer(double x)
+{
+    return (std::rand() * x / RAND_MAX < 1);//详细分析见《C Primer Plus》程序清单17.9
+}
 #endif
