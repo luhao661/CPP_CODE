3fb1c2e8ab54573c7a8abd6d842623a5067b456c
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-11-05 19:33:13 +0800

    Add files via upload

--- C++ Primer Plus Code practice/chapter 15/code1/Project1/Project1/code1.cpp
+++ C++ Primer Plus Code practice/chapter 15/code1/Project1/Project1/code1.cpp
@@ -541,8 +541,89 @@ int main()
 #endif
 
 
-//
+//程序清单15.16 类，异常，继承
 //链接cxqd15.15.cpp
 #if 1
+#include "cxqd15.14.h"
 
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::endl;
+
+    double vals1[12] =
+    {
+        1220, 1100, 1122, 2212, 1232, 2334,
+        2884, 2393, 3302, 2922, 3002, 3544
+    };
+
+    double vals2[12] =
+    {
+        12, 11, 22, 21, 32, 34,
+        28, 29, 33, 29, 32, 35
+    };
+
+    Sales sales1(2011, vals1, 12);
+    LabeledSales sales2("Blogstar", 2012, vals2, 12);
+
+    cout << "First try block:\n";
+
+    try
+    {
+        int i;
+        cout << "Year = " << sales1.Year() << endl;
+        for (i = 0; i < 12; ++i)
+        {
+
+            cout << sales1[i] << ' ';
+            if (i % 6 == 5)
+                cout << endl;
+        }
+        cout << "Year = " << sales2.Year() << endl;
+        cout << "Label = " << sales2.Label() << endl;
+        for (i = 0; i <= 12; ++i)
+        {
+
+            cout << sales2[i] << ' ';//当i为12时，索引值超界，引发异常
+            if (i % 6 == 5)
+                cout << endl;
+        }
+        cout << "End of try block 1.\n";
+    }
+    catch (LabeledSales::nbad_index& bad)//***注***两个catch块的顺序不能颠倒，P511
+    {
+        cout << bad.what();
+        cout << "Company: " << bad.label_val() << endl;
+        cout << "bad index: " << bad.bi_val() << endl;
+    }
+    catch (Sales::bad_index& bad)
+    {
+        cout << bad.what();
+        cout << "bad index: " << bad.bi_val() << endl;
+    }
+
+    cout << "\nNext try block:\n";
+    
+    try
+    {
+        sales2[2] = 37.5;
+        sales1[20] = 23345;//***注***此处索引值超界，operator[]()引发异常
+        cout << "End of try block 2.\n";
+    }
+    catch (LabeledSales::nbad_index& bad)
+    {
+        cout << bad.what();
+        cout << "Company: " << bad.label_val() << endl;
+        cout << "bad index: " << bad.bi_val() << endl;
+    }
+    catch (Sales::bad_index& bad)
+    {
+        cout << bad.what();
+        cout << "bad index: " << bad.bi_val() << endl;
+    }
+    cout << "done\n";
+    // std::cin.get();
+    return 0;
+}
 #endif
--- C++ Primer Plus Code practice/chapter 15/code1/Project1/Project1/cxqd15.14.h
+++ C++ Primer Plus Code practice/chapter 15/code1/Project1/Project1/cxqd15.14.h
@@ -3,7 +3,7 @@
 #ifndef CXQD_15_14_H
 #define CXQD_15_14_H
 // sales.h  -- exceptions and inheritance
-#include <stdexcept>
+#include <stdexcept>//声明了logic_error异常类系列和runtime_error异常类系列
 #include <string>
 
 class Sales
@@ -12,11 +12,13 @@ public:
     enum { MONTHS = 12 };   // could be a static const
     //也可放在private中
 private:
-    double gross[MONTHS];
     int year;
+    double gross[MONTHS];
 
 public:
-    class bad_index : public std::logic_error
+    //嵌套类
+    /**************************************************************************/
+    class bad_index : public std::logic_error//逻辑错误中的索引值错误
     {
     private:
         int bi;  // bad index value
@@ -24,39 +26,62 @@ public:
     public:
         explicit bad_index(int ix,
             const std::string& s = "Index error in Sales object\n");
-        int bi_val() const { return bi; }
-        virtual ~bad_index() throw() {}
+        int bi_val() const
+        {
+            return bi; 
+        }
+        //virtual ~bad_index() throw() {} //C++98的功能：异常规范
+        //或写为：(C++11已不使用异常规范)
+        virtual ~bad_index() 
+        {}
     };
+    /**************************************************************************/
 
-    explicit Sales(int yy = 0);
+    explicit Sales(int yy = 0);//构造函数，默认构造函数
     Sales(int yy, const double* gr, int n);
-    virtual ~Sales() { }
-    int Year() const { return year; }
+
+    int Year() const
+    {
+        return year; 
+    }
     virtual double operator[](int i) const;
     virtual double& operator[](int i);
+    virtual ~Sales()
+    {}
 };
 
 class LabeledSales : public Sales
 {
+private:
+    std::string label;
 public:
-    class nbad_index : public Sales::bad_index
+    /**************************************************************************/
+    class nbad_index : public Sales::bad_index//对嵌套类进行继承
     {
     private:
         std::string lbl;
     public:
         nbad_index(const std::string& lb, int ix,
             const std::string& s = "Index error in LabeledSales object\n");
-        const std::string& label_val() const { return lbl; }
+        const std::string& label_val() const
+        {
+            return lbl; 
+        }
         virtual ~nbad_index() throw() {}
     };
+    /**************************************************************************/
+
     explicit LabeledSales(const std::string& lb = "none", int yy = 0);
     LabeledSales(const std::string& lb, int yy, const double* gr, int n);
-    virtual ~LabeledSales() { }
-    const std::string& Label() const { return label; }
+    
+    const std::string& Label() const
+    {
+        return label; 
+    }
     virtual double operator[](int i) const;
     virtual double& operator[](int i);
-private:
-    std::string label;
+    virtual ~LabeledSales()
+    {}
 };
 
 #endif /*CXQD_15_14_H*/
