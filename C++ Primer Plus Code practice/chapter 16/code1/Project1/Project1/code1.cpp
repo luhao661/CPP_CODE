b66ea411e6207838eab7d8554355a7b66aad3c8f
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-11-30 19:34:19 +0800

    Add files via upload

--- C++ Primer Plus Code practice/chapter 16/code1/Project1/Project1/code1.cpp
+++ C++ Primer Plus Code practice/chapter 16/code1/Project1/Project1/code1.cpp
@@ -763,3 +763,96 @@ int main()
     return 0;
 }
 #endif
+
+
+//程序清单16.12 使用insert()、remove()、splice()、
+//unique()、sort()、merge()方法
+//链接无
+#if 1
+#include <iterator>
+#include <list>//成员函数insert()等
+#include <algorithm>//STL函数(非成员函数)：for_each()
+
+void outint(int n) 
+{
+    std::cout << n << " "; 
+}
+
+int main()
+{
+    using namespace std;
+
+    //创建一个由5个int类型的值2组成的序列
+    list<int> one(5, 2); // list of 5 2s
+
+    cout << "List one: ";
+    for_each(one.begin(), one.end(), outint);
+
+    //创建一个空序列
+    list<int> two;
+
+    //将整个stuff数组的内容插入到two的首元素前
+    int stuff[5] = { 1,2,4,8, 6 };
+    //法一：(使用insert()成员函数)
+    two.insert(two.begin(), stuff, stuff + 5);
+    //法二：(使用insert_iterator插入迭代器)
+    /*copy(stuff,stuff+5,
+        insert_iterator<list<int>>(two,two.end()));*/
+    
+    cout << endl << "List two: ";
+    for_each(two.begin(), two.end(), outint);
+    
+    //调用复制构造函数进行类型复制
+    list<int> three(two);
+
+    int more[6] = { 6, 4, 2, 4, 6, 5 };
+    three.insert(three.end(), more, more + 6);
+    
+    cout << endl << "List three: ";
+    for_each(three.begin(), three.end(), outint);
+    
+    //从双向链表中删除值为2的所有实例
+    three.remove(2);
+    cout << endl << "List three (删除了值为2的实例): ";
+    for_each(three.begin(), three.end(), outint);
+
+    //将链表one的内容插入到three头结点的前面
+    //***注***one将清空
+    three.splice(three.begin(), one);
+    cout << endl << "List three after splice: ";
+    for_each(three.begin(), three.end(), outint);
+    cout << endl << "List one: ";
+    for_each(one.begin(), one.end(), outint);
+
+    //将连续的相同元素压缩为单个元素
+    three.unique();
+    cout << endl << "List three after unique: ";
+    for_each(three.begin(), three.end(), outint);
+
+    //排序(默认递增)
+    three.sort();
+    //***注***
+    //不能使用STL函数sort()，
+    //因为非成员函数sort()需要随机访问迭代器，
+    //而list不支持随机访问
+    //sort(three.begin(),three.end());
+
+    //合并同类项
+    three.unique();
+    cout << endl << "List three after sort & unique: ";
+    for_each(three.begin(), three.end(), outint);
+
+    //排序(默认递增)
+    two.sort();
+    //将两个链表合并
+    //***注***前提：两个链表必须已经排序
+    three.merge(two);
+    cout << endl << "Sorted two merged into three: ";
+    for_each(three.begin(), three.end(), outint);
+
+    cout << endl;
+    // cin.get();
+
+    return 0;
+}
+#endif
