94ecc2aca979962b59c2a31fd48299703264a2f3
luhao661 <102971130+luhao661@users.noreply.github.com>
2023-01-02 19:31:13 +0800

    Add files via upload

--- Dev-C++/code7/code7.cpp
+++ Dev-C++/code7/code7.cpp
@@ -1,9 +1,11 @@
 //7.1 动态规划
 //将待求解的问题分解成若干子问题，按顺序求解子问题 
 //前一个子问题的解为后一个子问题的求解提供了有用的信息
+//建立状态转移方程
+//考虑初始条件和边界情况 
 
 //兑换硬币问题 
-#if 1
+#if 0
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -28,9 +30,72 @@ int main()
 				cc[i]=min(cc[i-a[j]]+1,cc[i]);//algorithm头文件中的min() 
 		}
 	}
-	
+	lower_bound
 	cout<<cc[21]<<endl;
 
 	return 0;
 }
 #endif 
+//***注***
+//algorithm头文件中实用的函数：
+//min()、max()、abs()
+//swap()、reverse()、sort() 
+//find()、upper_bound()、lower_bound()
+//fill()、count()、foreach() 
+//__gcd()
+//set_intersection()、set_union()、set_difference() 
+//next_permutation()
+
+//0-1背包问题 
+#if 1
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+	int num_items,num_weight;
+	
+	int value[100][100];
+	int w[100],v[100];//存重量，存价值
+
+	cin>>num_items>>num_weight;
+	
+	for(int i=0;i<num_items;i++)
+		cin>>w[i]>>v[i];
+		
+	fill(value[0],&value[0][num_weight],0); 
+
+//	for(int i=0;i<=num_weight;i++)
+//		cout<<value[0][i]<<" ";
+
+	
+	
+	return 0;
+} 
+#endif
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
